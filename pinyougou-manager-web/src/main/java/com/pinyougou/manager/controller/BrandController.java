b7512e20c5b0224647343d62585f975efc9f7970
阿诚 <mr_acheng@163.com>
2019-03-04 08:46:30 +0800

    初次提交

---
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -0,0 +1,28 @@
+package com.pinyougou.manager.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.IBrandService;
+
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+	
+	@Reference
+	private IBrandService brandService;
+	
+	@RequestMapping("/findAll")
+	private List<TbBrand> findAll(){
+		
+		
+		return brandService.findAll();
+	}
+	
+
+}
---
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/IBrandService.java
@@ -0,0 +1,16 @@
+package com.pinyougou.sellergoods.service;
+
+import java.util.List;
+
+import com.pinyougou.pojo.TbBrand;
+
+/**
+ * 品牌接口
+ * @author huangcheng
+ *
+ */
+public interface IBrandService {
+	
+	public List<TbBrand> findAll();
+
+}
